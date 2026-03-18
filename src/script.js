f38ce21175ab8577bc27da63d9df663d63223baa
kuukuawobil <kukuawobil@icloud.com>
2026-03-18 13:58:51 -0200

    created for the final part

--- src/script.js
+++ src/script.js
@@ -5,3 +5,5 @@ let age= prompt("How old are you?");
 alert("You are " + age + " years old. That's great!");
 let email = prompt("What is your email address?");
 alert("Your email address is " + email);
+
+alert( " Hi " +  +name + ", you are " + age + " years old and we will keep in touch with you via your email: " + email + "👋🏽👋🏽"); 
