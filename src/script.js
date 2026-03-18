9d6ffdd57b18f648fc240bcc2b1aa484e28209ab
kuukuawobil <kukuawobil@icloud.com>
2026-03-18 13:24:08 -0200

    created prompt for users to add their age

--- src/script.js
+++ src/script.js
@@ -1,2 +1,5 @@
 let name = prompt("What is your name?");
 alert("Hello, " + name + "! Welcome to PromptJS.");
+
+let age= prompt("How old are you?");
+alert("You are " + age + " years old. That's great!");
