fb3fad3b6280b4b8ea89b538311613b01685834e
zainabhabib2095-art <zainabhabib2095@gmail.com>
2026-03-18 09:38:11 -0400

    created a prompt for users to enter their email

--- src/script.js
+++ src/script.js
@@ -3,3 +3,5 @@ alert("Hello, " + name + "! Welcome to PromptJS.");
 
 let age= prompt("How old are you?");
 alert("You are " + age + " years old. That's great!");
+let email = prompt("What is your email address?");
+alert("Your email address is " + email);
